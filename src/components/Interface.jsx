7d19c2d8d2da8796b3a06a922bd3a026cfa369ae
Artur Burlakin <ab226ij@student.lnu.se>
2024-07-25 00:59:22 +0200

    made sections

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,26 +1,68 @@
 const Section = (props) => {
   const { children } = props;
-  return <Selection>{children}</Selection>;
+  return (
+    <selection
+      className={"h-screen w-screen p-16 max-w-screen-2xl max-auto flex flex-col items-start justify-center"}
+    >
+      {children}
+    </selection>
+  );
 };
 
 export const Interface = () => {
   return (
-    <>
-      <Section>
-        <h1>Culture Connection!</h1>
-      </Section>
-      <Section>
-        <h1>Events</h1>
-      </Section>
-      <Section>
-        <h1>Social Media</h1>
-      </Section>
-      <Section>
-        <h1>WeeklyCard</h1>
-      </Section>
-      <Section>
-        <h1>Sponsorse</h1>
-      </Section>
-    </>
+    <div className="flex flex-col items-center w-screen">
+      <TopSection />
+      <EventSection />
+      <SocialmediaSection />
+      <WeeklySection />
+      <SponsorseSection />
+    </div>
+  );
+};
+
+const TopSection = () => {
+  return (
+    <Section>
+      <h1 className=" bg-slate-500 text-4xl font-bold text-gray-900 mb-4">
+        Culture Connection!
+      </h1>
+      <p className="text-lg text-gray-700 mb-6">
+        Discover and engage with diverse cultures <br />
+        from around the world. Join us to explore, <br />
+        learn, and connect through shared experiences and stories.
+      </p>
+      <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+        Buy Subscription
+      </button>
+    </Section>
+  );
+};
+const EventSection = () => {
+  return (
+    <Section>
+      <h1>Event</h1>
+    </Section>
+  );
+};
+const SocialmediaSection = () => {
+  return (
+    <Section>
+      <h1>Social Media</h1>
+    </Section>
+  );
+};
+const WeeklySection = () => {
+  return (
+    <Section>
+      <h1>Weekly event cards</h1>
+    </Section>
+  );
+};
+const SponsorseSection = () => {
+  return (
+    <Section>
+      <h1>Sponsorse Planets</h1>
+    </Section>
   );
 };
