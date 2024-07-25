9b573a91ae639fa9e28d825205f11d526c181af2
Artur Burlakin <ab226ij@student.lnu.se>
2024-07-25 12:45:53 +0200

    adding animation to the section

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -6,7 +6,11 @@ const Section = (props) => {
     <motion.section
       className="h-screen w-screen p-16 max-w-screen-2xl mx-auto flex flex-col items-start justify-center"
       initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0, transition: { duration: 1.5, delay: 0.5} }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: { duration: 1.5, delay: 0.5 },
+      }}
     >
       {children}
     </motion.section>
@@ -28,17 +32,41 @@ export const Interface = () => {
 const TopSection = () => {
   return (
     <Section>
-      <h1 className=" bg-slate-500 text-4xl font-bold text-gray-900 mb-4">
+      <motion.h1
+        className=" bg-slate-500 text-4xl font-bold text-gray-900 mb-4"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+          transition: { duration: 1.5, delay: 0.3 },
+        }}
+      >
         Culture Connection!
-      </h1>
-      <p className="text-lg text-gray-700 mb-6">
+      </motion.h1>
+      <motion.p
+        className="text-lg text-gray-700 mb-6"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+          transition: { duration: 1.5, delay: 0.6 },
+        }}
+      >
         Discover and engage with diverse cultures <br />
         from around the world. Join us to explore, <br />
         learn, and connect through shared experiences and stories.
-      </p>
-      <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+      </motion.p>
+      <motion.button
+        className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+          transition: { duration: 1.5, delay: 0.9 },
+        }}
+      >
         Buy Subscription
-      </button>
+      </motion.button>
     </Section>
   );
 };
