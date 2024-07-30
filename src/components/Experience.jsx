298cb408635bc233c7aef1eb7239e0b83ab7ffe9
Artur Burlakin <ab226ij@student.lnu.se>
2024-07-30 21:42:50 +0200

    adjust orbit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Model } from "./CultureConnectionModel";
 import { motion } from "framer-motion-3d";
 import OrbitingSphere from "./OrbitingSphere";
+import { CatmullRomCurve3, Vector3 } from "three";
+import { Line } from "@react-three/drei";
 
 const pageNames = ["Events", "Social Media", "Weekly", "Sponsors"];
 const colors = ["#FF5733", "#33FF57", "#3357FF", "#FF33A1"];
-const orbitRadius = [1.5, 1.8, 2.1, 2.4]; // Reduced distances
+const orbitRadius = [1.6, 2.3, 3, 3.7]; // Semi-major axis lengths
+const orbitMinorRadius = [1.0, 1.2, 1.4, 1.6]; // Semi-minor axis lengths
+const speeds = [0.00085, 0.00087, 0.00089, 0.00081]; // Different speeds for variety
+const heightVariations = [2.5, 3.2, 3.6, 4.3]; // Different height variations for 3D effect
 
 const Experience = ({ section }) => {
+  const curves = useMemo(() => {
+    return pageNames.map((_, index) => {
+      const points = new Array(100).fill(0).map((_, i) => {
+        const angle = (i / 100) * Math.PI * 2;
+        const x = orbitRadius[index] * Math.cos(angle);
+        const z = orbitMinorRadius[index] * Math.sin(angle);
+        const y = heightVariations[index] * Math.sin(angle); // Adding height variation for 3D effect
+        return new Vector3(x, y, z);
+      });
+      return new CatmullRomCurve3(points, true);
+    });
+  }, []);
+
   return (
     <>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.8} />
       <motion.group
         position={[0, 0, 0]}
         scale={[0.5, 0.5, 0.5]}
@@ -27,14 +45,20 @@ const Experience = ({ section }) => {
       </motion.group>
 
       {pageNames.map((name, index) => (
-        <OrbitingSphere
-          key={index}
-          name={name}
-          index={index}
-          color={colors[index]}
-          radius={orbitRadius[index]}
-          length={pageNames.length}
-        />
+        <React.Fragment key={index}>
+          <Line
+            points={curves[index].getPoints(100)}
+            color={colors[index]}
+            lineWidth={1}
+          />
+          <OrbitingSphere
+            name={name}
+            index={index}
+            color={colors[index]}
+            curve={curves[index]}
+            speed={speeds[index]}
+          />
+        </React.Fragment>
       ))}
     </>
   );
